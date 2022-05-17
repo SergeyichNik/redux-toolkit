e8d12c5dd48bb455ae8a5cc5f1b8afe5c9189d30
SergeichNik <tyunev.nik89@gmail.com>
2022-05-17 21:57:02 +0300

    add App structure & uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from "uuid";
+
+type TodoType = {
+    id: string,
+    text: string,
+    completed: boolean
+}
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [todos, setTodos] = useState<TodoType[]>([])
+    const [text, setText] = useState('')
+
+    const addTodo = () => {
+        if (text.trim().length) {
+            setTodos([
+                ...todos,
+                {
+                    id: v1(),
+                    text,
+                    completed: false
+                }
+            ])
+            setText('')
+        }
+
+    }
+
+    return (
+        <div className="App">
+            <label htmlFor="">
+                <input value={text} onChange={(e ) => setText(e.currentTarget.value)}/>
+                <button onClick={addTodo}>Add</button>
+            </label>
+            <ul>
+                {
+                    todos.map(todo => <li key={todo.id}>
+                        <input type="checkbox"/>
+                        <span>{todo.text}</span>
+                        <span style={{color: 'red'}}>&times;</span>
+                    </li>)
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
