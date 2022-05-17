1cb9373303f26f73c5922aa698b722d91d8b72ff
SergeichNik <tyunev.nik89@gmail.com>
2022-05-18 00:18:11 +0300

    change some

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,34 +11,17 @@ const TodoList = () => {
     const todos = useSelector<RootStateType, TodoType[]>((state) => state.todos)
 
     const displayTodos = todos.map(todo => <TodoItem {...todo}
-                                                     toggleStatusTodo={() => {}}
+                                                     toggleStatusTodo={() => {
+                                                     }}
                                                      key={todo.id}
-                                                     removeTodo={() => {}}/>)
-
-import React, {FC} from 'react';
-import {TodoType} from "../App";
-import TodoItem from "./TodoItem";
-
-type PropsType = {
-    todos: TodoType[],
-    toggleStatusTodo: (id: string) => void,
-    removeTodo: (id: string) => void
-}
-
-const TodoList: FC<PropsType> = ({todos, removeTodo, toggleStatusTodo}) => {
-
-    const displayTodos = todos.map(todo => <TodoItem {...todo}
-                                                     toggleStatusTodo={toggleStatusTodo}
-                                                     key={todo.id}
-                                                     removeTodo={removeTodo}/>
->>>>>>> origin/main
-    )
-
+                                                     removeTodo={() => {
+                                                     }}/>)
     return (
         <ul>
             {displayTodos}
         </ul>
-    );
-};
+    )
 
+
+}
 export default TodoList;
